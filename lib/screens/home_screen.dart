61b5dd24b9a147f58af1bdc45a32671c3f74df9e
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-01 11:15:12 +0430

    Add search widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,94 @@
 import 'package:flutter/material.dart';
+import '../customized_libs/search_widget.dart';
 
-class HomeScreen extends StatelessWidget {
-  final Function callback;
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
-  const HomeScreen(this.callback);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showLogo = true;
+  TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: new Image.asset('assets/Beheshti.png', width: 150.0),
+        flexibleSpace: Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: AnimSearchBar(
+                    width: MediaQuery.of(context).size.width - 40,
+                    rtl: true,
+                    helpText: "جستجو",
+                    closeSearchOnSuffixTap: true,
+                    autoFocus: true,
+                    style: TextStyle(
+                        fontFamily: 'Beheshti',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 17,
+                        color: Colors.black
+                    ),
+                    // autoFocus: true,
+                    textController: textController,
+                    onSuffixTap: () {
+                      setState(() {
+                        showLogo = false;
+                      });
+                      textController.clear();
+                    },
+                    onCloseBar: () {
+                      setState(() {
+                        showLogo = true;
+                      });
+                    }
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: AnimatedOpacity(
+                  opacity: showLogo ? 1.0 : 0.0,
+                  duration: const Duration(milliseconds: 100),
+                  child: Image.asset('assets/Beheshti.png', width: 150.0),
+                ),
+              )
+            ],
+          ),
+        ),
         toolbarHeight: 100,
-        centerTitle: true,
-        leadingWidth: 10,
         backgroundColor: Colors.white,
         elevation: 0,
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () => callback(),
-          child: Text('Go to Page3'),
-        ),
-      ),
+      body: Body(),
+    );
+  }
+}
+class Body extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        Text("Home screen")
+      ],
     );
   }
 }
