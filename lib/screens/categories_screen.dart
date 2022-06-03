c99027a9ed00708723b7cd6074184b5aad157c97
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-03 10:54:56 +0430

    Change animation of categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -102,7 +102,7 @@ class Body extends StatelessWidget {
           position: index,
           duration: const Duration(milliseconds: 375),
           child: SlideAnimation(
-            verticalOffset: 50.0,
+            verticalOffset: -50.0,
             child: FadeInAnimation(
               child: Container(
                   decoration: BoxDecoration(
