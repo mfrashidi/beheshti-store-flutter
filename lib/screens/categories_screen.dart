110fa316082deaf1669ea871665c5e401f1891be
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-04 10:03:31 +0430

    Make categories screen stateful

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,41 +1,18 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({Key? key}) : super(key: key);
+List<Map<String, dynamic>> categories = [];
 
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: AnimationLimiter(
-        child: Container(
-          child: Body(),
-          padding: EdgeInsets.symmetric(horizontal: 25),
-        ),
-      ),
-      appBar: AppBar(
-          title: Text(
-              "دسته بندی کالاها",
-              style: TextStyle(
-                  fontFamily: 'Beheshti',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.black
-              )
-          ),
-          toolbarHeight: 100,
-          centerTitle: true,
-          leadingWidth: 10,
-          backgroundColor: Colors.white.withOpacity(0.3),
-          elevation: 0
-      )
-    );
-  }
+  _CategoriesScreenState createState() => _CategoriesScreenState();
 }
 
-
-class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+class _CategoriesScreenState extends State<CategoriesScreen> {
 
   ShaderMask fadeImage(String path) {
     return ShaderMask(
@@ -55,101 +32,113 @@ class Body extends StatelessWidget {
     );
   }
 
+  Future<void> _getCategories() async {
+    final String response = await rootBundle.loadString(
+        'assets/categories.json');
+    final data = await json.decode(response);
+    setState(() {
+      Map<String, dynamic> categoriesMap = data;
+      categoriesMap.forEach((key, value) {
+        categories.add(value);
+      });
+    });
+  }
+
   @override
-  Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
-      {"pic": "assets/screens/categories/Macbook.png",
-        "text": "دیجیتال",
-        "color": Color(0x9CA2F0FF),
-        "fade": false,
-        "padding": 30.0,
-        "width": 125.0
-      },
-      {"pic": "assets/screens/categories/clothing.png",
-        "text": "پوشاک",
-        "color": Color(0xFFFFA0A0),
-        "fade": true,
-        "padding": 10.0,
-        "width": 100.0
-      },
-      {"pic": "assets/screens/categories/Books.png",
-        "text": "کتاب و لوازم التحریر",
-        "color": Color(0xA2F0FF9C),
-        "fade": false,
-        "padding": 20.0,
-        "width": 100.0
-      },
-      {"pic": "assets/screens/categories/Tent.png",
-        "text": "ورزش و سفر",
-        "color": Color(0xA2A7FFC4),
-        "fade": false,
-        "padding": 5.0,
-        "width": 100.0
-      },
-    ];
-    return GridView.builder(
-      physics: BouncingScrollPhysics(),
-      shrinkWrap: true,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 0.95,
-        crossAxisSpacing: 30,
-        mainAxisSpacing: 30,
-      ),
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        return AnimationConfiguration.staggeredList(
-          position: index,
-          duration: const Duration(milliseconds: 375),
-          child: SlideAnimation(
-            verticalOffset: -50.0,
-            child: FadeInAnimation(
-              child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        categories[index]["color"],
-                        Colors.white.withOpacity(0.25),
-                      ],
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        color: Colors.black.withOpacity(.1),
-                      )
-                    ],
-                  ),
-                  child:
-                  Stack(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.topCenter,
-                        child: Padding(
-                          padding: EdgeInsets.only(top: categories[index]["padding"]),
-                          child: categories[index]["fade"] ? fadeImage(categories[index]["pic"]) : Image.asset(categories[index]["pic"], width: categories[index]["width"]),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                            padding: const EdgeInsets.only(bottom: 20),
-                            child: Text(
-                                categories[index]["text"],
-                                style: TextStyle(fontFamily: 'Beheshti', fontWeight: FontWeight.bold, fontSize: 15)
-                            )
-                        ),
-                      )
-                    ],
-                  )
+  void initState() {
+    super.initState();
+    if (categories.isEmpty) _getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        body: AnimationLimiter(
+          child: categories.isEmpty ? Container() : Container(
+            child: GridView.builder(
+              physics: BouncingScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.95,
+                crossAxisSpacing: 30,
+                mainAxisSpacing: 30,
               ),
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return AnimationConfiguration.staggeredList(
+                  position: index,
+                  duration: const Duration(milliseconds: 375),
+                  child: SlideAnimation(
+                    verticalOffset: -50.0,
+                    child: FadeInAnimation(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Color(int.parse(categories[index]["color"])),
+                                Colors.white.withOpacity(0.25),
+                              ],
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(.1),
+                              )
+                            ],
+                          ),
+                          child:
+                          Stack(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Padding(
+                                  padding: EdgeInsets.only(top: categories[index]["image_padding"]),
+                                  child: categories[index]["has_fade_image"] ? fadeImage(categories[index]["image"]) : Image.asset(categories[index]["image"], width: categories[index]["image_width"]),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Padding(
+                                    padding: const EdgeInsets.only(bottom: 20),
+                                    child: Text(
+                                        categories[index]["name"],
+                                        style: TextStyle(fontFamily: 'Beheshti', fontWeight: FontWeight.bold, fontSize: 15)
+                                    )
+                                ),
+                              )
+                            ],
+                          )
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
+            padding: EdgeInsets.symmetric(horizontal: 25),
           ),
-        );
-      },
+        ),
+        appBar: AppBar(
+            title: Text(
+                "دسته بندی کالاها",
+                style: TextStyle(
+                    fontFamily: 'Beheshti',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.black
+                )
+            ),
+            toolbarHeight: 100,
+            centerTitle: true,
+            leadingWidth: 10,
+            backgroundColor: Colors.white.withOpacity(0.3),
+            elevation: 0
+        )
     );
   }
 }
+
   
