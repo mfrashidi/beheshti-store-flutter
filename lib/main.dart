b79e425daaba1ba79d48569babc236cbb04d1ad4
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-06-04 11:46:29 +0430

    Chnage navigation bar to remain after changing pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'package:custom_navigator/custom_scaffold.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nama_kala/screens/cart_screen.dart';
 import 'package:nama_kala/screens/categories_screen.dart';
 import 'package:nama_kala/screens/profile_screen.dart';
 import 'screens/home_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
-TextStyle appBarFont = GoogleFonts.notoSansArabic(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold);
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -27,12 +27,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   int activeIndex = 0;
-  void changeActivePage(int index) {
-    setState(() {
-      activeIndex = index;
-    });
-  }
 
   List<Widget> pages = [];
 
@@ -51,66 +47,31 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     double _iconSize = 30;
     MaterialColor _inactiveIconColor = Colors.grey;
+    Color _activeIconColor = Colors.black;
     Color _iconBackgroundColor = Colors.white.withOpacity(0);
-    return Scaffold(
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.75),
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 20,
-                color: Colors.black.withOpacity(.1),
-              )
-            ],
-          ),
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
-              child: GNav(
-                rippleColor: Colors.grey[300]!,
-                hoverColor: Colors.grey[100]!,
-                gap: 8,
-                activeColor: Colors.black,
-                iconSize: 24,
-                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                duration: Duration(milliseconds: 300),
-                tabBackgroundColor: Colors.grey[100]!,
-                color: Colors.black,
-                tabs: [
-                  GButton(
-                    icon: LineIcons.store,
-                    iconSize: _iconSize,
-                    iconColor: _inactiveIconColor,
-                    backgroundColor: _iconBackgroundColor,
-                  ),
-                  GButton(
-                    icon: LineIcons.compass,
-                    iconSize: _iconSize,
-                    iconColor: _inactiveIconColor,
-                    backgroundColor: _iconBackgroundColor,
-                  ),
-                  GButton(
-                    icon: LineIcons.shoppingCart,
-                    iconSize: _iconSize,
-                    iconColor: _inactiveIconColor,
-                    backgroundColor: _iconBackgroundColor,
-                  ),
-                  GButton(
-                    icon: LineIcons.user,
-                    iconSize: _iconSize,
-                    iconColor: _inactiveIconColor,
-                    backgroundColor: _iconBackgroundColor,
-                  ),
-                ],
-                selectedIndex: activeIndex,
-                onTabChange: (index) {
-                  changeActivePage(index);
-                  })
-              ),
-            ),
-          ),
-      body: pages[activeIndex],
-        );
+
+    final _items = [
+      BottomNavigationBarItem(icon: Icon(LineIcons.store, color: activeIndex == 0 ? _activeIconColor : _inactiveIconColor, size: _iconSize), label: 'خانه'),
+      BottomNavigationBarItem(icon: Icon(LineIcons.compass, color: activeIndex == 1 ? _activeIconColor : _inactiveIconColor, size: _iconSize), label: 'دسته‌بندی ها'),
+      BottomNavigationBarItem(icon: Icon(LineIcons.shoppingCart, color: activeIndex == 2 ? _activeIconColor : _inactiveIconColor, size: _iconSize), label: 'سبد خرید'),
+      BottomNavigationBarItem(icon: Icon(LineIcons.user, color: activeIndex == 3 ? _activeIconColor : _inactiveIconColor, size: _iconSize), label: 'پروفایل'),
+    ];
+
+    return CustomScaffold(
+      scaffold: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.white,
+          items: _items,
+        ),
+      ),
+      children: pages,
+      onItemTap: (index) {
+        setState(() {
+          activeIndex = index;
+        });
+      },
+    );
   }
 }
