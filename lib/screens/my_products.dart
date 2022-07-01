9d1a3a2732a4d9d31b783a42d9f101dc88fe987f
mfrashidi <mohammadfoadrashidi@gmail.com>
2022-07-01 05:08:03 +0430

    Update my products screen

--- lib/screens/my_products.dart
+++ lib/screens/my_products.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +9,14 @@ import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:nama_kala/screens/add_new_product_screen.dart';
 import 'package:nama_kala/screens/product_screen.dart';
+import 'package:nama_kala/screens/profile_screen.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../assets/item_card.dart';
 
 Map<String, dynamic> products = {};
 Map<String, dynamic> user = {};
+List<String> productIds = [];
 
 class MyProductScreen extends StatefulWidget {
   const MyProductScreen({Key? key}) : super(key: key);
@@ -21,88 +26,110 @@ class MyProductScreen extends StatefulWidget {
 
 class _MyProductScreenState extends State<MyProductScreen> {
 
-  Future<void> _getProducts() async {
-    final String response = await rootBundle.loadString('assets/products.json');
-    final data = await json.decode(response);
-    setState(() {
-      products = data;
+  Future<void> _getMyProducts() async {
+    await Socket.connect("192.168.1.7", 4536).then((serverSocket) async {
+      String token = await getToken() ?? "nothing";
+      serverSocket.write("AUTH=" + token + ";GET_MY_PRODUCTS\n");
+      serverSocket.flush();
+      serverSocket.listen((response) async {
+        final data = await json.decode(utf8.decode(response));
+        productIds = [];
+        setState(() {
+          for (var i in data) {
+            productIds.add(i.toString());
+          }
+        });
+      });
     });
   }
 
-  Future<void> _getUser() async {
-    final String response = await rootBundle.loadString('assets/user.json');
-    final data = await json.decode(response);
-    setState(() {
-      user = data;
+  Future<Map<String, dynamic>> getProduct(String productId) async {
+    Completer<Map<String, dynamic>> _completer = Completer<Map<String, dynamic>>();
+    await Socket.connect("192.168.1.7", 4536).then((serverSocket) async {
+      String token = await getToken() ?? "nothing";
+      serverSocket.write("AUTH=" + token + ";GET_PRODUCT=" + productId + "\n");
+      serverSocket.flush();
+      serverSocket.listen((response) async {
+        _completer.complete(json.decode(utf8.decode(response)));
+      });
     });
+
+    return _completer.future;
   }
 
   @override
   void initState() {
     super.initState();
-    _getProducts();
-    _getUser();
+    _getMyProducts();
   }
 
   Widget _myProducts() {
-    Map<String, dynamic> filteredProducts = Map.from(products)..removeWhere((k, v) => v["owner"] != user["user_id"]);
-    List<String> productIds = [];
-    for (var k in filteredProducts.keys) {
-      productIds.add(k);
-    }
-    return productIds.isEmpty ? Container() : ListView(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: [
-        Container(
-          height: 250,
-          child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              physics: BouncingScrollPhysics(),
-              padding: EdgeInsets.symmetric(vertical: 20),
-              itemCount: productIds.length,
-              itemBuilder: (context, index) {
-                return AnimationConfiguration.staggeredList(
-                  position: index,
-                  duration: const Duration(milliseconds: 375),
-                  child: Padding(
-                      padding: EdgeInsets.only(right: index == 0 ? 25 : 5, left: index == productIds.length - 1 ? 25 : 5),
-                      child: SlideAnimation(
-                        horizontalOffset: 50.0,
-                        child: FadeInAnimation(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(CupertinoPageRoute(builder: (context) => AddNewProductScreen(products[productIds[index]])));
-                              },
-                              child: Container(
-                                width: 165,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.05),
-                                      spreadRadius: 0,
-                                      blurRadius: 10,
-                                      offset: Offset(0, 10), // changes position of shadow
-                                    ),
-                                  ],
-                                ),
-                                padding: EdgeInsets.all(10),
-                                child: products.isNotEmpty ? getItemCard(
-                                    products[productIds[index]]["image"],
-                                    products[productIds[index]]["name"],
-                                    products[productIds[index]]["price"]) : Container(),
-                              ),
-                            )
-                        ),
-                      )),
-                );
-              }
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: GridView.builder(
+          physics: BouncingScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.8,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 20,
           ),
-        ),
-        SizedBox(height: 30)
-      ],
+          itemCount: productIds.length,
+          itemBuilder: (context, index) {
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 375),
+              child: Padding(
+                  padding: EdgeInsets.all(0),
+                  child:  FutureBuilder<Map<String, dynamic>>(
+                      future: getProduct(productIds[index]),
+                      builder: (context, AsyncSnapshot<Map<String, dynamic>> product) {
+                        return SlideAnimation(
+                          horizontalOffset: 50.0,
+                          child: FadeInAnimation(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(CupertinoPageRoute(builder: (context) => AddNewProductScreen(product.data!)));
+                                },
+                                child: Container(
+                                  width: 165,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.05),
+                                        spreadRadius: 0,
+                                        blurRadius: 10,
+                                        offset: Offset(0, 10), // changes position of shadow
+                                      ),
+                                    ],
+                                  ),
+                                  padding: EdgeInsets.all(10),
+                                  child: product.hasData ? getItemCard(
+                                      product.data!["image"],
+                                      product.data!["name"],
+                                      product.data!["price"]) : SizedBox(
+                                    width: 165.0,
+                                    height: 250.0,
+                                    child: Shimmer.fromColors(
+                                        baseColor: Colors.white,
+                                        highlightColor: Colors.grey.shade100,
+                                        child: Container(
+                                          width: 500.0,
+                                          height: 500.0,
+                                          color: Colors.white,
+                                        )
+                                    ),
+                                  ),
+                                ),
+                              )
+                          ),
+                        );})),
+            );
+          }
+      ),
     );
   }
 
